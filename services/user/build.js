819174afeef0291c7b4275468d6aeaf65d7116c2
Eli Bates <eli@bateswebtech.com>
2023-03-19 08:45:10 +0100

    Add build scripts and configuration for esbuild in user and web-gateway services and update package.json files to include type: module and module: dist/index.mjs

---
+++ services/user/build.js
@@ -0,0 +1,18 @@
+import { build } from 'esbuild'
+
+try {
+  await build({
+    entryPoints: ['./src/index.ts'],
+    bundle: true,
+    outdir: './dist/',
+    sourcemap: true,
+    minify: true,
+    conditions: ['worker', 'browser'],
+    outExtension: { '.js': '.mjs' },
+    format: 'esm',
+    target: 'esnext',
+  })
+} catch (err) {
+  process.exitCode = 1
+}
+
